f1bb6b136c99878179fc60d2ae763bbdd6f0fca7
An Huynh <An Huynh>
2016-11-04 18:43:18 -0500

    done!

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,21 @@
 var humanPals = [
 {
-	name: 'Alison in Wonderland',
-	img: 'http://www.sharonsevents.com/image/data/alice/Alice-PNG-alice-in-wonderland-33922018-444-800.png',
+	name: 'Dorthy',
+	img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQHJ0RZGCcnggtRDEx2TmHviA7IxguBJdoCG1UkQBsTAWmArv-p',
 	response: [
 	'1: Strongly Disagree',
-	'2: Disagree', 
+	'4: Agree', 
 	'3: Neutral', 
 	'4: Agree', 
 	'5: Strongly Agree', 
 	'4: Agree', 
 	'3: Neutral', 
-	'2: Disagree', 
-	'1: Strongly Disagree', 
-	'1: Strongly Disagree']
+	'4: Agree', 
+	'5: Strongly Agree', 
+	'5: Strongly Agree', ]
 }, {
 	name: 'Rick Sanchez',
-	img: 'http://static1.comicvine.com/uploads/original/11122/111225835/5265754-rick.jpg',
+	img: 'http://67.media.tumblr.com/d53ac79772bb55603622dac0f6553efd/tumblr_nthozxxgKg1rsitj2o1_500.png',
 	response: [
 	'1: Strongly Disagree', 
 	'1: Strongly Disagree', 
@@ -28,19 +28,49 @@ var humanPals = [
 	'1: Strongly Disagree', 
 	'1: Strongly Disagree']
 }, {
-	name: 'Patrick Yang',
-	img: 'http://www.sharonsevents.com/image/data/alice/Alice-PNG-alice-in-wonderland-33922018-444-800.png',
+	name: 'Stewie Griffin',
+	img: 'http://stuffpoint.com/family-guy/image/55462-family-guy-brian-and-stewie.jpg',
 	response: [
 	'1: Strongly Disagree',
 	'2: Disagree', 
 	'3: Neutral', 
-	'4: Agree', 
-	'5: Strongly Agree', 
-	'4: Agree', 
+	'2: Disagree', 
+	'1: Strongly Disagree',
+	'2: Disagree',  
 	'3: Neutral', 
 	'2: Disagree', 
 	'1: Strongly Disagree', 
 	'1: Strongly Disagree']
-}];
+},{
+	name: 'Morty Smith',
+	img: 'http://66.media.tumblr.com/52ab9bfd262943fae2275578d1d20b15/tumblr_inline_o91ycowg6L1sb58ss_1280.jpg',
+	response: [
+	'5: Strongly Agree',
+	'5: Strongly Agree',
+	'1: Strongly Disagree',
+	'1: Strongly Disagree',
+	'1: Strongly Disagree',
+	'1: Strongly Disagree',
+	'2: Disagree',
+	'1: Strongly Disagree',
+	'3: Neutral',
+	'1: Strongly Disagree',]
+},{
+	name: 'Jon Snow',
+	img: 'https://s-media-cache-ak0.pinimg.com/originals/d5/9c/3c/d59c3c50c553bbb022c49dcc7e521656.jpg',
+	response: [
+	'5: Strongly Agree',
+	'5: Strongly Agree',
+	'4: Agree',
+	'5: Strongly Agree',
+	'2: Disagree',
+	'5: Strongly Agree',
+	'5: Strongly Agree',
+	'5: Strongly Agree',
+	'3: Neutral',
+	'5: Strongly Agree',
+	]}
+
+];
 
 module.exports  = humanPals;
