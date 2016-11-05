ed076b6f86a8dc17e9eabbbb6dec18a53fcde8bd
An Huynh <An Huynh>
2016-11-05 14:11:36 -0500

    finished

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,24 +1,16 @@
+//add validation, for the forms that force the user to pick the values from 1-5!
+
 
 var questions = [
-//1
 'Your mind is always buzzing with unexplored ideas and plans.', 
-//2
 'Generally speaking, you rely more on your experience than your imagination.', 
-//3
 'You find it easy to stay relaxed and focused even when there is some pressure.',
-//4
 'You rarely do something just out of sheer curiosity.',
-//5
 'People can rarely upset you.',
-//6
 'It is often difficult for you to relate to other people’s feelings.',
-//7
 'In a discussion, truth should be more important than people’s sensitivities.',
-//8
 'You rarely get carried away by fantasies and ideas.',
-//9
 'You think that everyone’s views should be respected regardless of whether they are supported by facts or not.',
-//10
 'You feel more energetic after spending time with a group of people.'
 ];
 
@@ -199,12 +191,4 @@ function showModal(bestMatchName, bestMatchLink){
 $('document').ready(function() {
 	createQuestions();
 	$('.submitBtn').on('click', getUserInfo);
-	//$('.submitBtn').on('click', showModal);
 });
-
-
-
-
-
-
-
